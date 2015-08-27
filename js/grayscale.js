dc0166cc38856baf3ad2463d908d32cb3a010b6b
bvassilis <bvassilis@gmail.com>
2015-08-27 14:13:04 +0200

    Modal Sign up, not scrolling closes #12

--- js/grayscale.js
+++ js/grayscale.js
@@ -78,3 +78,21 @@ $(function(){
         changeBackgoundImg(j);
     }, 800000);
 });
+
+// fix multi modal popup scrolling
+$(function(){
+    $(document).on('show.bs.modal', function (event) {
+        if (!event.relatedTarget) {
+            $('.modal').not(event.target).modal('hide');
+        };
+        if ($(event.relatedTarget).parents('.modal').length > 0) {
+            $(event.relatedTarget).parents('.modal').modal('hide');
+        };
+    });
+
+    $(document).on('shown.bs.modal', function (event) {
+        if ($('body').hasClass('modal-open') == false) {
+            $('body').addClass('modal-open');
+        };
+    });
+});
